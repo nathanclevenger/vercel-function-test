a2afe944689c1aed87ad929ef491cffb8aee5e26
Nathan Clevenger <nateclev@gmail.com>
2022-07-25 07:48:01 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 export default function handler(req, res) {
-  res.setHeader('Cache-Control', 's-maxage=86400');
-  res.status(200).json({ name: 'John Doe' });
+  res.setHeader('Cache-Control', 's-maxage=86400')
+  res.status(200).json({ name: 'John Doe', req })
 }
